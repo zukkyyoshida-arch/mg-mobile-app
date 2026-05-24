bc0ede5966f5d4ad3b2fe64387377ddffff516a2
Kazuki Yoshida <kazuki@example.com>
2026-05-25 08:52:13 +0900

    Use MemoryRouter for complete iframe safety

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { MemoryRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import Dashboard from './components/Dashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <MemoryRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </HashRouter>
+    </MemoryRouter>
   </StrictMode>,
 )
